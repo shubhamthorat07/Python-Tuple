351c9c192281e077e75b708f6dec7911a5754c27
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-09-07 14:58:25 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,39 @@
+t = (1,2,3,4,5)
+print(t)
+print(type(t))
+
+
+
+t1 = (10,20,30,40,50)
+print(t1[3])
+print(t1[1:4])
+
+
+
+
+t2 = (100,200,200,200,300,300,400,500)
+print(t2[0:])
+print(t2[0:5])
+print(t2.count(200))
+print(t2.count(300))
+
+
+
+
+l = [6,7,8,9]
+t3 = tuple(l)
+print(t3)
+print(type(t3))
+
+
+
+
+t4 = (1000,2000,3000,4000)
+l1 = list(t4)
+print(l1)
+print(type(l1))
+
+
+
+
+
